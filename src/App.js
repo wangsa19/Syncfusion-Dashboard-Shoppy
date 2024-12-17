8f703ca7a8cbe60142400c10155206d067c0287c
wangsa19 <dewanggawpw@gmail.com>
2024-12-17 11:37:59 +0700

    Datatables

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ const App = () => {
                                 {/* Pages */}
                                 <Route path='/orders' element={<Orders />} />
                                 <Route path='/employees' element={<Employees />} />
-                                <Route path='/customer' element={<Customers />} />
+                                <Route path='/customers' element={<Customers />} />
                                 {/* Apps */}
                                 <Route path='/kanban' element={<Kanban />} />
                                 <Route path='/editor' element={<Editor />} />
